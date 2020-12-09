54a7897d683356fdfa2219753ce4e9b8e3ff4301
Artur Gussi <arturgussi@gmail.com>
2020-12-09 11:04:34 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import { useState } from 'react';
+import Link from 'next/link'
 
 function Home() {
   return (
     <div>
       <h1>Página Teste Inicial</h1>
+
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
+
       <Contador />
     </div>
   )
